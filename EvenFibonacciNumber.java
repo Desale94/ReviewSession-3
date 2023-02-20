2925feeb0900f54699b48b3eba9a37db0ad61494
Desale1 <dkdesale08@gmail.com>
2023-02-20 09:46:04 +0530

    Print nth even fibonacci number

---
+++ EvenFibonacciNumber.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.Example;
+
+import com.jogamp.nativewindow.ScalableSurface;
+
+import java.util.Scanner;
+
+public class EvenFibonacciNumber {
+    public static void main(String[] args) {
+        int n1=0,n2=1;
+        int n3,i;//count=10;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the count number: ");
+        int count=s.nextInt();
+        System.out.println(n1+"\n"+n2);
+        for (i = 2; i < count; i++){
+            n3=n1+n2;
+            if (n3 % 2 == 0)
+                System.out.println(""+n3);
+            n1=n2;
+            n2=n3;
+        }
+
+    }
+}
